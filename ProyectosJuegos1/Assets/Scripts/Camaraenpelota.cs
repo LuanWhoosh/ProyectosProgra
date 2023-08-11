92ab0e4954778ebb1197afa475edc889b2383e37
LuanWhoosh <142039106+LuanWhoosh@users.noreply.github.com>
2023-08-11 11:44:05 -0500

    Primera versiòn del proyecto

---
+++ ProyectosJuegos1/Assets/Scripts/Camaraenpelota.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camaraenpelota : MonoBehaviour
+{
+    //atributo fisico publico
+    public Transform Pelota;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Pelota.position.x,
+            Pelota.position.y,
+            -1);
+        
+    }
+}
