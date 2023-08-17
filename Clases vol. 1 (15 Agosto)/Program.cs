e8908d775cd261ab15c77ae1d11c401081fb41d6
LuanWhoosh <142039106+LuanWhoosh@users.noreply.github.com>
2023-08-17 01:58:28 -0600

    ClasesinicialesSISIRVE

---
+++ Clases vol. 1 (15 Agosto)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Clases_vol._1__15_Agosto_
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //punto de inicio del programa
+            DispositivoMovil miIphone = new DispositivoMovil();
+            miIphone.almacenamiento = 64;
+            miIphone.ram = 16;
+            miIphone.resHeight = 1080;
+            miIphone.resWidth = 1920;
+            miIphone.color = new Color();
+            miIphone.color.rojo = 123;
+            miIphone.color.verde = 56;
+            miIphone.color.azul = 146;
+
+            DispositivoMovil tabletSamsung = new DispositivoMovil();
+            tabletSamsung.almacenamiento = 256;
+            tabletSamsung.ram = 32;
+            tabletSamsung.resHeight = 1080;
+            tabletSamsung.resWidth = 1920;
+            tabletSamsung.color = new Color();
+            tabletSamsung.color.rojo = 249;
+            tabletSamsung.color.verde = 231;
+            tabletSamsung.color.azul = 84;
+
+
+
+        }
+
+    }
+}
