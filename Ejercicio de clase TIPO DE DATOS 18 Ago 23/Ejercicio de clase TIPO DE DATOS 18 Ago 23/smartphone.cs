1452761a1a361a975f500c358fd7886a29e097b8
LuanWhoosh <142039106+LuanWhoosh@users.noreply.github.com>
2023-08-18 12:16:57 -0500

    Ejercicio de clase avanzado falta Program

---
+++ Ejercicio de clase TIPO DE DATOS 18 Ago 23/Ejercicio de clase TIPO DE DATOS 18 Ago 23/smartphone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Ejercicio_de_clase_TIPO_DE_DATOS_18_Ago_23
+{
+    class smartphone
+    {
+        public string NetworkTech;
+        public int LaunchAnnounced;
+        public string LaunchStatus;
+        public int BodyDimensionAlto;
+        public float BodyDimensionLargo;
+        public int BodyDimensionAncho;
+        public int BodyWeigth;
+        public string BodySIM;
+        public string DisplayType;
+        public int DisplayTypeNits;
+        public float DisplaySize;
+        public int DisplayResolutionAncho;
+        public int DisplayResolutionAlto;
+        public string PlatformOS;
+        public string PlatforChipset;
+        public string PlatformCPU;
+        public string PlatformGPU;
+        public string MemoryCardslot;
+        public int MemoryInternal;
+        public int MemoryInternalRAM;
+        public int MainCameraTriple1;
+        public int MainCameraTriple2;
+        public int MainCameraTriple3;
+        public string MainCameraFeatures;
+        public int MainCameraVideoP;
+        public int MainCameraVideoFps;
+        public int SelfieCameraSingle;
+        public string SelfieCameraFeatures;
+        public int SelfieCameraVideoP;
+        public int SelfieCameraVideoFps;
+        public bool SoundLoudspeaker;
+        public bool Sound3_5Jack;
+        public string CommsWlan;
+        public int CommsBluetooth;
+        public string CommsPositioning;
+        public bool CommsNFC;
+        public string CommsRadio;
+        public string CommsUSB;
+        public string FeaturesSensors;
+        public string BatteryType;
+        public int BatteryCharging;
+        public string MiscColors;
+        public string MiscModels;
+        public float MiscSar;
+        public float MiscSarEu;
+        public float MiscPrice; 
+    }
+}
