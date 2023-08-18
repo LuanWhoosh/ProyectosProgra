261d941a4148569f90dddd72107ffa1e95a72166
LuanWhoosh <142039106+LuanWhoosh@users.noreply.github.com>
2023-08-18 12:55:59 -0500

    Carrera de coches terminada

---
+++ CarreraDeCoches/Auto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace CarreraDeCoches
+{
+    public class Auto
+    {
+        public string modelo;
+        public Color color;
+        public float velocidad;
+    }
+}
---
+++ CarreraDeCoches/PistaCarreras.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace CarreraDeCoches
+{
+    public class PistaCarreras
+    {
+        public string nombre;
+        public float longitud;
+
+
+    //Es un metodo que recibe dos parametros 
+        public void Competir(Auto auto1, Auto auto2)
+         {
+           Console.WriteLine("Iniciando competencia entre   "  + auto1.modelo + "  contra  " + auto2.modelo);
+
+            if (auto1.velocidad > auto2.velocidad)
+            {
+                Console.WriteLine("Gano " + auto1.modelo);
+            }
+            else if (auto2.velocidad > auto1.velocidad)
+            {
+                Console.WriteLine("Gano " + auto2.modelo);
+            }
+            else
+            {
+                Console.WriteLine("Hubo un empate");
+            }
+     
+
+}
+
+
+}
+
+
+}
