658c7f0bf7128521d230086d957c84da70ac911a
LuanWhoosh <142039106+LuanWhoosh@users.noreply.github.com>
2023-08-25 01:30:46 -0600

    tipos de datos

---
+++ TipodeDatosEjerciciodeClases/TipodeDatosEjerciciodeClases/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace TipodeDatosEjerciciodeClases
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone Xiaomi9C = new Smartphone();
+            Xiaomi9C.NetworkTech = "GSM";
+            Xiaomi9C.LaunchAnnounced = 2020;
+            Xiaomi9C.LaunchStatus = "Available";
+            Xiaomi9C.BodyDimensionsHeight = 9f;
+            Xiaomi9C.BodyDimensionsLength = 164.9f;
+            Xiaomi9C.BodyDimensionsWidth = 77f;
+            Xiaomi9C.BodyWeight = 196;
+            Xiaomi9C.BodySIM = "Dual SIM";
+            Xiaomi9C.DisplayType = "IPS LCD";
+            Xiaomi9C.DisplaySize = 6.53f;
+            Xiaomi9C.ResolutionLength = 1600;
+            Xiaomi9C.ResolutionWidth = 720;
+            Xiaomi9C.PlatformOS = "Android 10";
+            Xiaomi9C.PlatformChipset = "Mediatek MT6765G Helio G35 ";
+            Xiaomi9C.PlatformCPU = "Octa-core";
+            Xiaomi9C.PlatformGPU = "PowerVR GE8320";
+            Xiaomi9C.MemoryCarddSlot = "microSDXC";
+            Xiaomi9C.MemoryInternal = 32;
+            Xiaomi9C.MainCameraTriple = 13;
+            Xiaomi9C.MainCameraFeatures = "LED flash";
+            Xiaomi9C.MainCameraVideo = 1080;
+            Xiaomi9C.SelfieCameraSingle = 5;
+            Xiaomi9C.SelfieCameraFeatures = "HDR";
+            Xiaomi9C.SelfieCameraVideo = 1080;
+            Xiaomi9C.SoundLoudspeaker = true;
+            Xiaomi9C.Jack = true;
+            Xiaomi9C.CommsWLAN = "Wi-Fi 802.11 b/g/n";
+            Xiaomi9C.CommsBluetooth = 5;
+            Xiaomi9C.CommsPositioning = "GPS";
+            Xiaomi9C.CommsNFC = false;
+            Xiaomi9C.CommsRadio = "FM Radio";
+            Xiaomi9C.CommsUSB = "microUSB 2.0,";
+            Xiaomi9C.FeaturesSensors = "Fingerprint";
+            Xiaomi9C.BatteryType = "Li-Po 5000 mAh";
+            Xiaomi9C.BatteryCharging = "10W wired";
+            Xiaomi9C.MiscColors = "Midnight Gray";
+            Xiaomi9C.MiscModels = "M2006C3MG";
+            Xiaomi9C.MiscSar = 0.73f;
+            Xiaomi9C.MiscSarEu = 0.35f;
+            Xiaomi9C.MiscPrice = 82f;
+        }
+
+    }
+}
---
+++ TipodeDatosEjerciciodeClases/TipodeDatosEjerciciodeClases/Smartphone.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace TipodeDatosEjerciciodeClases
+{
+    public class Smartphone
+    {
+        public string NetworkTech;
+        public int LaunchAnnounced;
+        public string LaunchStatus;
+        public float BodyDimensionsHeight;
+        public float BodyDimensionsLength;
+        public float BodyDimensionsWidth;
+        public int BodyWeight;
+        public string BodySIM;
+        public string DisplayType;
+        public float DisplaySize;
+        public int ResolutionLength;
+        public int ResolutionWidth;
+        public string PlatformOS;
+        public string PlatformChipset;
+        public string PlatformCPU;
+        public string PlatformGPU;
+        public string MemoryCarddSlot;
+        public int MemoryInternal;
+        public int MainCameraTriple;
+        public string MainCameraFeatures;
+        public int MainCameraVideo;
+        public int SelfieCameraSingle;
+        public string SelfieCameraFeatures;
+        public int SelfieCameraVideo;
+        public bool SoundLoudspeaker;
+        public bool Jack;
+        public string CommsWLAN;
+        public int CommsBluetooth;
+        public string CommsPositioning;
+        public bool CommsNFC;
+        public string CommsRadio;
+        public string CommsUSB;
+        public string FeaturesSensors;
+        public string BatteryType;
+        public string BatteryCharging;
+        public string MiscColors;
+        public string MiscModels;
+        public float MiscSar;
+        public float MiscSarEu;
+        public float MiscPrice;
+    }
+}
