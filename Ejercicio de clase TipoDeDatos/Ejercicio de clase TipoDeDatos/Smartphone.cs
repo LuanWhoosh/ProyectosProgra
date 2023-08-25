582ff44b0e672188acc3a7e200d382de19e0662d
LuanWhoosh <142039106+LuanWhoosh@users.noreply.github.com>
2023-08-24 23:22:49 -0600

    tarea fallida

---
+++ Ejercicio de clase TipoDeDatos/Ejercicio de clase TipoDeDatos/Smartphone.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ejercicio_de_clase_TipoDeDatos
+{
+   public class Smartphone
+    {
+    }
+}
---
+++ Ejercicio de clase TipoDeDatos/Smartphone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio_de_clase_TipoDeDatos
+{
+   public class Smartphone
+    {
+        public string NetworkTech;
+        public int LaunchAnnounced;
+        public float BodyDimensionsHeight;
+        public float BodyDimensionsLength;
+        public float BodyDimensionsWidth;
+        public int BodyWeight;
+        public string BodySIM;
+        public string DisplayType;
+        public float DisplaySize;
+
+    }
+}
