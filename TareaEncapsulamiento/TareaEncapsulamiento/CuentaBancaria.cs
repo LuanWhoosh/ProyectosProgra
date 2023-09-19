d4cb47b3442241c3c5cd0a95515a031e8a9a734e
LuanWhoosh <142039106+LuanWhoosh@users.noreply.github.com>
2023-09-18 23:03:05 -0600

    tarea encapsulamiento

---
+++ TareaEncapsulamiento/TareaEncapsulamiento/CuentaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TareaEncapsulamiento
+{
+    class CuentaBancaria
+    {
+        private int pin;
+        private int saldo;
+        public int getPin()
+        {
+            return pin;
+        }
+        public int getSaldo()
+        {
+            if (pin != 0)
+            {
+                return saldo;
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Saldo no disponible");
+                return 0;
+            }
+        }
+
+        public void setPin(int valor)
+        {
+            pin = valor;
+        }
+
+        public void setSaldo(int cantidad)
+        {
+            saldo = cantidad;
+        }
+
+        
+    }
+}
---
+++ TareaEncapsulamiento/TareaEncapsulamiento/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TareaEncapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Consulta de saldo de Cuenta Bancaria");
+
+            CuentaBancaria cuenta = new CuentaBancaria();
+
+            cuenta.setPin(2828);
+            cuenta.setSaldo(5000);
+
+            int pin = cuenta.getPin();
+            int saldo = cuenta.getSaldo();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Tu PIN es: " + pin);
+            Console.WriteLine(" ");
+            Console.WriteLine("Saldo total de: $ " + saldo);
+            Console.ReadLine();
+        }
+    }
+}
