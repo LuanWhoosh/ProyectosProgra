a13b6e6f1702f290c250dacb4f0f4e0a2f1a47de
LuanWhoosh <andyml0704@gmail.com>
2023-09-19 14:38:27 -0600

    AIA Plataforma

--- Plataformero/Assets/Scripts/ControladorJugador.cs
+++ Plataformero/Assets/Scripts/ControladorJugador.cs
@@ -5,39 +5,48 @@ using UnityEngine;
 public class ControladorJugador : MonoBehaviour
 {
     public float velocidadCaminar = 5;
-    private Rigidbody2D MiCuerpo;
-    private Animator MiAnimador;
+    public float fuerzaSalto = 20;
+    private Rigidbody2D miCuerpo;
+    private Animator miAnimador;
     // Start is called before the first frame update
     void Start()
     {
-        MiCuerpo = GetComponent<Rigidbody2D>();
-        MiAnimador = GetComponent<Animator>();
+        miCuerpo = GetComponent<Rigidbody2D>();
+        miAnimador = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        float velVert = MiCuerpo.velocity.y;
+        float velVert = miCuerpo.velocity.y;
         float movHoriz = Input.GetAxis("Horizontal");
+       
 
         if (movHoriz > 0)
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
-            MiCuerpo.velocity = new Vector3(velocidadCaminar, velVert, 0);
-            MiAnimador.SetBool("CAMINANDO", true);
+            miCuerpo.velocity = new Vector3(velocidadCaminar, velVert, 0);
+            miAnimador.SetBool("CAMINANDO", true);
         }
         else if (movHoriz < 0)
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
-            MiCuerpo.velocity = new Vector3(-velocidadCaminar, velVert,0);
-            MiAnimador.SetBool("CAMINANDO", true);
+            miCuerpo.velocity = new Vector3(-velocidadCaminar, velVert,0);
+            miAnimador.SetBool("CAMINANDO", true);
         }
         else
         {
-            MiCuerpo.velocity = new Vector3(0, velVert, 0);
-            MiAnimador.SetBool("CAMINANDO", false);
+            miCuerpo.velocity = new Vector3(0, velVert, 0);
+            miAnimador.SetBool("CAMINANDO", false);
         }
         
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            miCuerpo.AddForce(new Vector3(0, fuerzaSalto, 0) , ForceMode2D.Impulse);
+        }
+
+        miAnimador.SetFloat("VEL_VERT", velVert);
     }
 }
