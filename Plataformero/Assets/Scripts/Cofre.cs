8e66b78f6d9c42e8f98d6d23ecea6adb3841827f
LuanWhoosh <142039106+LuanWhoosh@users.noreply.github.com>
2023-11-10 11:59:42 -0600

    tarea AAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Plataformero/Assets/Scripts/Cofre.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cofre : MonoBehaviour
+{
+    private Animator miAnimador;
+    public GameObject boton;
+    void Start()
+    {
+        miAnimador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject otro = collision.gameObject;
+        print("El " + name + " colisiona con " + collision);
+
+        if (otro.tag == "Player")
+        {
+            
+            miAnimador.SetTrigger("TOMAR");
+            boton.SetActive(true);
+            pressBoton();
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GameObject otro = collision.gameObject;
+        print("El" + name + " colisiona con " + collision);
+
+        if (otro.tag == "Player")
+        {
+
+            miAnimador.SetTrigger("TOMAR");
+            boton.SetActive(false);
+            pressBoton();
+
+        }
+    }
+
+    public void pressBoton()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            print("Se presiono AAA");
+            
+        }
+    }
+
+
+
+}
